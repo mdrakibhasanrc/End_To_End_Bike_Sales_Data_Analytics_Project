631e5b6d72e9072d29d04ed5d76a5aace962b578
Md Rakib Hasan <103329593+mdrakibhasanrc@users.noreply.github.com>
2023-05-14 15:24:52 +0600

    Add files via upload

---
+++ 5. Bike_Sales_Sql_Exploration_Part-1.sql
@@ -0,0 +1,304 @@
+-- Create View or Temporary Table -- Table Name: Analytics Query
+
+create or replace table `sqlproject-379113.bike_sales.analytics_query` as(select
+    a.Date,
+    a.Year,
+    a.Day_Name,
+    a.Month_Name,
+    a.Weekend_Flag,
+    b.Customer_Age,
+    b.Customer_Gender,
+    b.Customer_Age_Group,
+    c.State,
+    c.Country,
+    d.Product_Category,
+    d.Sub_Category,
+    d.Product,
+    e.Order_Quantity,
+    e.Unit_Cost,
+    e.Unit_Price,
+    e.Cost,
+    f.Profit,
+    f.Revenue
+from `sqlproject-379113.bike_sales.Bike_date_dim` a
+join `sqlproject-379113.bike_sales.Bike_Customr_dim` b on a.Date_Id=b.Customer_Id
+join `sqlproject-379113.bike_sales.Bike_demographic_dim` c on a.Date_Id=c.Demographic_Id
+join `sqlproject-379113.bike_sales.bike_product_dim` d on a.Date_Id=d.Product_Id
+join `sqlproject-379113.bike_sales.bike_order_dim` e on a.Date_Id=e.Order_info_Id
+join `sqlproject-379113.bike_sales.fact_table` f on a.Date_Id=f.Order_Id);
+
+
+-- Total Row Number
+select
+    count(*) as total_row
+from 
+    `sqlproject-379113.bike_sales.analytics_query`;
+
+-- Total Revenue
+select
+    sum(Revenue) as total_Revenue
+from 
+    `sqlproject-379113.bike_sales.analytics_query`;
+
+   
+-- Total Revenue
+select
+    avg(Revenue) as avg_Revenue
+from 
+    `sqlproject-379113.bike_sales.analytics_query`;
+
+   
+-- Total Profit
+select
+    sum(Profit) as avg_Revenue
+from 
+    `sqlproject-379113.bike_sales.analytics_query`;
+
+   
+-- Total Profit
+select
+    round(avg(Profit),2) as avg_Profit
+from 
+    `sqlproject-379113.bike_sales.analytics_query`;
+
+-- Avg Age 
+select
+    avg(Customer_Age) as avg_Age
+from 
+    `sqlproject-379113.bike_sales.analytics_query`;
+
+-- Profit Margin
+select
+    (sum(Profit)/sum(Revenue)*100) as Profit_Margin
+from
+  `sqlproject-379113.bike_sales.analytics_query`;
+
+
+-- Gender by Revenue\\
+
+select
+    Customer_Gender,
+    count(Customer_Gender) as cnt,
+    sum(Revenue) as Total_Sales
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by Customer_Gender
+order by Total_Sales ,cnt desc;
+
+-- Gender by Profit\\
+
+select
+    Customer_Gender,
+    count(Customer_Gender) as cnt,
+    sum(Profit) as Total_Profit
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by Customer_Gender
+order by Total_Profit,cnt desc;
+
+-- Customer Age Group by Revenue\\
+
+select
+    Customer_Age_Group,
+    count(Customer_Age_Group) as cnt,
+    sum(Revenue) as Total_Sales
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by Customer_Age_Group
+order by Total_Sales desc ,cnt desc;
+
+-- Gender by Profit\\
+
+select
+    Customer_Age_Group,
+    count(Customer_Age_Group) as cnt,
+    sum(Profit) as Total_Profit
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by Customer_Age_Group
+order by Total_Profit desc,cnt desc;
+
+-- Year by Profit
+select
+   Year,
+    sum(Revenue) as Total_Sales
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by
+      Year
+order by
+     Total_Sales desc;
+
+
+-- Year by Profit
+select
+   Year,
+    sum(Profit) as Total_Profit
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by
+      Year
+order by
+     Total_Profit desc;
+
+
+
+-- Day by Revenue
+select
+   Day_Name,
+    sum(Revenue) as Total_Sales
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by
+      Day_Name
+order by
+     Total_Sales desc;
+
+
+-- Day by Profit
+select
+   Day_Name,
+    sum(Profit) as Total_Profit
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by
+    Day_Name
+order by
+     Total_Profit desc;
+
+
+
+-- Country USA vs Revenue
+select
+   Country,
+   Product_Category,
+   sum(Revenue) as total_sales
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+where country="United States"
+group by
+     Country,Product_Category
+order by
+     total_sales desc;
+
+
+-- Weekend Flag Profit
+
+select
+    Weekend_Flag,
+    count(Weekend_Flag) as cnt,
+    sum(Profit) as total_Profit
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+where Year=2022
+group by
+     Weekend_Flag;
+
+
+select
+    Country,
+    Customer_Age_Group,
+    Product_Category,
+    sum(Revenue) as total_Revenue
+from `sqlproject-379113.bike_sales.analytics_query`
+group by 
+    Country,
+    Customer_Age_Group,
+    Product_Category
+   order by
+        total_Revenue desc;
+
+-- Top Proudct Category by Revenue
+
+select
+     Product_Category,
+     sum(Revenue) as Revenue
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by Product_category
+order by Revenue desc
+limit 1;
+
+-- Top Proudct Category by Profit
+
+select
+     Product_Category,
+     sum(Profit) as Profit
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by Product_category
+order by Profit desc
+limit 1;
+
+
+-- Bottom 10  Proudct  by Revenue
+
+select
+     Product,
+     sum(Revenue) as Revenue
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by Product
+order by Revenue Asc
+limit 10;
+
+
+-- Top 10  Proudct  by Revenue
+
+select
+     Product,
+     count(Product) as cnt,
+     sum(Revenue) as Revenue
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by
+     Product
+order by
+     Revenue desc,cnt desc
+limit 10;
+
+
+-- Top 5 Sub_category  by Revenue
+
+select
+     Sub_category,
+     count(Sub_category) as cnt,
+     sum(Revenue) as Revenue
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by
+     Sub_category
+order by
+     Revenue desc,cnt desc
+limit 5;
+
+
+-- Bottom 5 Sub_category  by Revenue
+
+select
+     Sub_category,
+     count(Sub_category) as cnt,
+     sum(Revenue) as Revenue
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by
+     Sub_category
+order by
+     Revenue asc
+limit 5;
+
+-- Bottom 5 Sub_category  by Profit
+
+select
+     Sub_category,
+     count(Sub_category) as cnt,
+     sum(Profit) as Profit
+from
+   `sqlproject-379113.bike_sales.analytics_query`
+group by
+     Sub_category
+order by
+     Profit asc
+limit 5;
+
+
